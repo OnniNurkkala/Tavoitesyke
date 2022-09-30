d18767f169f1e7cd6ed64f835b02ed7ed07aaf80
OnniNurkkala <t0nuon00@students.oamk.fi>
2022-09-30 10:17:09 +0300

    bmi calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+
+  function laske(e){
+    e.preventDefault();
+    const tulos = weight / (height * height);
+    //setBMI = tulos;
+    setBMI(tulos)
+  }
+
   return (
     <div>
-      
+      <div>
+        <h1>Calculating body mass index</h1>
+      </div>
+      <form onSubmit={laske} >
+        <div>
+          <label>Height</label>
+          <input value={height} onChange={e => setHeight(e.target.value)}/>
+        </div>
+        <div>
+          <label>Weight</label>
+          <input value={weight} onChange={e => setWeight(e.target.value)}/>
+        </div>
+        <div>
+          <output>{bmi.toFixed(2)}</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
     </div>
   );
 }
